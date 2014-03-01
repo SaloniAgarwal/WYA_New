8a8739f3bf05d85af2443638bbdfa279d07342f1
basaktaylan <bt815@nyu.edu>
2014-03-01 16:20:33 -0500

    03-01-2014

---
+++ AddFriends.h
@@ -0,0 +1,13 @@
+//
+//  AddFriends.h
+//  WhereYouApp
+//
+//  Created by Basak Taylan on 2/28/14.
+//  Copyright (c) 2014 Timothy Chu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddFriends : UIViewController
+
+@end
---
+++ AddFriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddFriendsViewController.h
+//  WhereYouApp
+//
+//  Created by Basak Taylan on 3/1/14.
+//  Copyright (c) 2014 Timothy Chu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddFriendsViewController : UITableViewController
+-(IBAction)unwindToList:(UIStoryboardSegue *)segue;
+@end
