01f1e7897612cafaebadbab49302c00ded1aeafd
Saloni <saloni.agarwal@nyu.edu>
2014-03-01 19:02:24 -0500

    ADD GROUPS PROTOTYPE

---
+++ WYAAddGroupsViewController.h
@@ -0,0 +1,16 @@
+//
+//  WYAAddGroupsViewController.h
+//  WhereYouApp
+//
+//  Created by Saloni Agarwal on 3/1/14.
+//  Copyright (c) 2014 Timothy Chu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "WYAGroups.h"
+
+@interface WYAAddGroupsViewController : UIViewController
+
+@property WYAGroups *Group;
+
+@end
